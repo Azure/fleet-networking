37b432dace8431f6c8fe744396a4d6b551fc9887
michaelawyu <chenyu1@microsoft.com>
2022-06-07 13:47:13 +0800

    Minor fixes

--- controllers/serviceexport/doc.go
+++
@@ -1,3 +0,0 @@
-// Package serviceexport features the ServiceExport controller for syncing Services and EndpointSlices
-// between member clusters and the hub cluster in a fleet.
-package serviceexport
