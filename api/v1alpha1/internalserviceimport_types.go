807f3820bb8163fbdaf6cd7e6956bd083d123ed0
Qingchuan Hao <qinhao@microsoft.com>
2022-06-17 17:20:59 +0800

    feat: Add ServiceImport and InternalServiceImport api (#20)
    * feat: Add ServiceImport and InternalServiceImport api
    
    /kind feature
    /kind api-change
    
    Define ServiceImport and InternalServiceImport API, referring to upstream serviceimport

---
+++ api/v1alpha1/internalserviceimport_types.go
@@ -0,0 +1,43 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:object:root=true
+
+// InternalServiceImport is used by the MCS controller to import the Service to a single cluster manually,
+// while ServiceImport is logical identifiers for a Service that exists in another cluster or that stretches
+// across multiple clusters and it will be automatically created in the hub cluster when exporting service.
+type InternalServiceImport struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// status contains information about the exported services that form
+	// the multi-cluster service referenced by this ServiceImport.
+	// +optional
+	Status ServiceImportStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// InternalServiceImportList represents a list of InternalServiceImport.
+type InternalServiceImportList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard list metadata.
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	// List of InternalServiceImport.
+	// +listType=set
+	Items []InternalServiceImport `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&InternalServiceImport{}, &InternalServiceImportList{})
+}
---
+++ api/v1alpha1/serviceimport_types.go
@@ -0,0 +1,128 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package v1alpha1
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+// +kubebuilder:object:root=true
+
+// ServiceImport describes a service imported from clusters in a ClusterSet.
+type ServiceImport struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// status contains information about the exported services that form
+	// the multi-cluster service referenced by this ServiceImport.
+	// +optional
+	Status ServiceImportStatus `json:"status,omitempty"`
+}
+
+// ServiceImportType designates the type of a ServiceImport
+type ServiceImportType string
+
+const (
+	// ClusterSetIP are only accessible via the ClusterSet IP.
+	ClusterSetIP ServiceImportType = "ClusterSetIP"
+	// Headless services allow backend pods to be addressed directly.
+	Headless ServiceImportType = "Headless"
+)
+
+// ServicePort represents the port on which the service is exposed.
+type ServicePort struct {
+	// The name of this port within the service. This must be a DNS_LABEL.
+	// All ports within a ServiceSpec must have unique names. When considering the endpoints for a Service,
+	// this must match the 'name' field in the EndpointPort.
+	// Optional if only one ServicePort is defined on this service.
+	// +optional
+	Name string `json:"name,omitempty"`
+
+	// The IP protocol for this port. Supports "TCP", "UDP", and "SCTP".
+	// Default is TCP.
+	// +optional
+	Protocol v1.Protocol `json:"protocol,omitempty"`
+
+	// The application protocol for this port.
+	// This field follows standard Kubernetes label syntax.
+	// Un-prefixed names are reserved for IANA standard service names (as per
+	// RFC-6335 and http://www.iana.org/assignments/service-names).
+	// Non-standard protocols should use prefixed names such as
+	// mycompany.com/my-custom-protocol.
+	// Field can be enabled with ServiceAppProtocol feature gate.
+	// +optional
+	AppProtocol *string `json:"appProtocol,omitempty"`
+
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=65535
+	// The port that will be exposed by this service.
+	Port int32 `json:"port"`
+
+	// The port to access on the pods targeted by the service.
+	// +optional
+	TargetPort intstr.IntOrString `json:"targetPort,omitempty"`
+}
+
+// ServiceImportStatus describes derived state of an imported service.
+type ServiceImportStatus struct {
+	// ip will be used as the VIP for this service when type is ClusterSetIP.
+	// +kubebuilder:validation:MaxItems:=1
+	// +optional
+	IPs []string `json:"ips,omitempty"`
+	// type defines the type of this service.
+	// Must be ClusterSetIP or Headless.
+	// +kubebuilder:validation:Enum=ClusterSetIP;Headless
+	Type ServiceImportType `json:"type"`
+	// Supports "ClientIP" and "None". Used to maintain session affinity.
+	// Enable client IP based session affinity.
+	// Must be ClientIP or None.
+	// Defaults to None.
+	// Ignored when type is Headless
+	// More info: https://kubernetes.io/docs/concepts/services-networking/service/#virtual-ips-and-service-proxies
+	// +optional
+	SessionAffinity v1.ServiceAffinity `json:"sessionAffinity,omitempty"`
+	// sessionAffinityConfig contains session affinity configuration.
+	// +optional
+	SessionAffinityConfig *v1.SessionAffinityConfig `json:"sessionAffinityConfig,omitempty"`
+
+	// +listType=atomic
+	Ports []ServicePort `json:"ports"`
+
+	// clusters is the list of exporting clusters from which this service was derived.
+	// +optional
+	// +patchStrategy=merge
+	// +patchMergeKey=cluster
+	// +listType=map
+	// +listMapKey=cluster
+	Clusters []ClusterStatus `json:"clusters,omitempty"`
+}
+
+// ClusterStatus contains service configuration mapped to a specific source cluster.
+type ClusterStatus struct {
+	// cluster is the name of the exporting cluster. Must be a valid RFC-1123 DNS label.
+	Cluster string `json:"cluster"`
+}
+
+// +kubebuilder:object:root=true
+
+// ServiceImportList contains a list of ServiceImport.
+type ServiceImportList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard list metadata.
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	// List of ServiceImport.
+	// +listType=set
+	Items []ServiceImport `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ServiceImport{}, &ServiceImportList{})
+}
