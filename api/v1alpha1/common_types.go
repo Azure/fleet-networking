d5c55ffc43693719579f36d5a934a46846bd2c88
michaelawyu <chenyu1@microsoft.com>
2022-06-21 15:46:09 +0800

    feat: add EndpointSliceExport API (#23)

---
+++ api/v1alpha1/common_types.go
@@ -0,0 +1,34 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package v1alpha1
+
+import "k8s.io/apimachinery/pkg/types"
+
+// ExportedObjectReference helps operators identify the source of an exported object, e.g. an EndpointSliceExport.
+// +structType=atomic
+type ExportedObjectReference struct {
+	// The ID of the cluster where the object is exported.
+	// +kubebuilder:validation:Required
+	ClusterID string `json:"clusterId"`
+	// The API version of the referred object.
+	// +optional
+	APIVersion string `json:"apiVersion,omitempty"`
+	// The kind of the referred object.
+	// +kubebuilder:validation:Required
+	Kind string `json:"kind"`
+	// The namespace of the referred object.
+	// +kubebuilder:validation:Required
+	Namespace string `json:"namespace"`
+	// The name of the referred object.
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+	// The resource version of the referred object.
+	// +kubebuilder:validation:Required
+	ResourceVersion string `json:"resourceVersion"`
+	// The UID of the referred object.
+	// +kubebuilder:validation:Required
+	UID types.UID `json:"uid"`
+}
