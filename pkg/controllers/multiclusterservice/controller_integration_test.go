422da2b2c293de57f6e6a4c0ac40cfa4e053b8d5
Zhiying Lin <zhiyingl456@gmail.com>
2022-07-01 14:46:00 +0800

    add integration test

---
+++ pkg/controllers/multiclusterservice/controller_integration_test.go
@@ -0,0 +1,252 @@
+package multiclusterservice
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	fleetnetv1alpha1 "go.goms.io/fleet-networking/api/v1alpha1"
+)
+
+var _ = Describe("Test MultiClusterService Controller", func() {
+	const (
+		timeout  = time.Second * 10
+		duration = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	BeforeEach(func() {
+		By("Create multiClusterService namespace")
+		ns := corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: testNamespace,
+			},
+		}
+		Expect(k8sClient.Create(ctx, &ns)).Should(Succeed())
+
+		By("Create fleet system namespace")
+		ns = corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: systemNamespace,
+			},
+		}
+		Expect(k8sClient.Create(ctx, &ns)).Should(Succeed())
+	})
+
+	AfterEach(func() {
+		By("delete multiClusterService namespace")
+		ns := corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: testNamespace,
+			},
+		}
+		Expect(k8sClient.Delete(ctx, &ns)).Should(Succeed())
+
+		By("delete the fleet system namespace")
+		ns = corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: systemNamespace,
+			},
+		}
+		Expect(k8sClient.Delete(ctx, &ns)).Should(Succeed())
+	})
+
+	Context("When creating new MultiClusterService", func() {
+		It("Should create service import and derived service", func() {
+			By("By creating a new MultiClusterService")
+			ctx := context.Background()
+			multiClusterService := multiClusterServiceForTest()
+			Expect(k8sClient.Create(ctx, multiClusterService)).Should(Succeed())
+
+			mcsLookupKey := types.NamespacedName{Name: testName, Namespace: testNamespace}
+			createdMultiClusterService := &fleetnetv1alpha1.MultiClusterService{}
+
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, mcsLookupKey, createdMultiClusterService); err != nil {
+					return false
+				}
+				return createdMultiClusterService.GetLabels()[multiClusterServiceLabelServiceImport] == testServiceName
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking mcs condition and expecting unknown")
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, mcsLookupKey, createdMultiClusterService); err != nil {
+					return false
+				}
+				expected := metav1.Condition{
+					Type:   string(fleetnetv1alpha1.MultiClusterServiceValid),
+					Status: metav1.ConditionUnknown,
+					Reason: conditionReasonUnknownServiceImport,
+				}
+				return len(createdMultiClusterService.Status.Conditions) == 1 &&
+					cmp.Equal(createdMultiClusterService.Status.Conditions[0], expected, cmpopts.IgnoreFields(metav1.Condition{}, "Message", "LastTransitionTime", "ObservedGeneration"))
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking derived service label")
+			_, ok := createdMultiClusterService.GetLabels()[multiClusterServiceLabelService]
+			Expect(ok).Should(BeFalse())
+
+			serviceImportLookupKey := types.NamespacedName{Name: testServiceName, Namespace: testNamespace}
+			createdServiceImport := &fleetnetv1alpha1.ServiceImport{}
+
+			By("By checking service import")
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, serviceImportLookupKey, createdServiceImport); err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking derived Service in the fleet-system")
+			Consistently(func() (int, error) {
+				serviceList := &corev1.ServiceList{}
+				if err := k8sClient.List(ctx, serviceList, &client.ListOptions{Namespace: systemNamespace}); err != nil {
+					return -1, err
+				}
+				return len(serviceList.Items), nil
+			}, duration, interval).Should(Equal(0))
+
+			By("By updating service import status")
+			createdServiceImport.Status = fleetnetv1alpha1.ServiceImportStatus{
+				Type: fleetnetv1alpha1.ClusterSetIP,
+				Clusters: []fleetnetv1alpha1.ClusterStatus{
+					{
+						Cluster: "member1",
+					},
+					{
+						Cluster: "member2",
+					},
+				},
+				Ports: []fleetnetv1alpha1.ServicePort{
+					{
+						Name:     "http",
+						Port:     8080,
+						Protocol: corev1.ProtocolTCP,
+					},
+					{
+						Name:     "https",
+						Port:     8843,
+						Protocol: corev1.ProtocolTCP,
+					},
+				},
+			}
+			Expect(k8sClient.Status().Update(ctx, createdServiceImport)).Should(Succeed())
+
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, mcsLookupKey, createdMultiClusterService); err != nil {
+					return false
+				}
+				return createdMultiClusterService.Status.Conditions[0].Status == metav1.ConditionTrue
+			}, duration, interval).Should(BeTrue())
+
+			derivedServiceLookupKey := types.NamespacedName{Name: createdMultiClusterService.GetLabels()[multiClusterServiceLabelService], Namespace: systemNamespace}
+			createdService := &corev1.Service{}
+			By("By checking derived service")
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, derivedServiceLookupKey, createdService); err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking mcs condition and expecting valid")
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, mcsLookupKey, createdMultiClusterService); err != nil {
+					return false
+				}
+				expected := metav1.Condition{
+					Type:   string(fleetnetv1alpha1.MultiClusterServiceValid),
+					Status: metav1.ConditionTrue,
+					Reason: conditionReasonFoundServiceImport,
+				}
+				option := cmpopts.IgnoreFields(metav1.Condition{}, "Message", "LastTransitionTime", "ObservedGeneration")
+				return len(createdMultiClusterService.Status.Conditions) == 1 &&
+					cmp.Equal(createdMultiClusterService.Status.Conditions[0], expected, option)
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking mcs load balancer status")
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, mcsLookupKey, createdMultiClusterService); err != nil {
+					return false
+				}
+				if err := k8sClient.Get(ctx, derivedServiceLookupKey, createdService); err != nil {
+					return false
+				}
+				return cmp.Equal(createdMultiClusterService.Status.LoadBalancer, createdService.Status.LoadBalancer)
+			}, timeout, interval).Should(BeTrue())
+
+			By("By updating mcs spec to use unknown service")
+			newServiceImport := "my-new-svc"
+			createdMultiClusterService.Spec.ServiceImport.Name = newServiceImport
+			Expect(k8sClient.Update(ctx, createdMultiClusterService)).Should(Succeed())
+
+			By("By checking derived service")
+			Eventually(func() bool {
+				return errors.IsNotFound(k8sClient.Get(ctx, derivedServiceLookupKey, createdService))
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking old service import")
+			Eventually(func() bool {
+				return errors.IsNotFound(k8sClient.Get(ctx, serviceImportLookupKey, createdServiceImport))
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking new service import")
+			newServiceImportLookupKey := types.NamespacedName{Name: newServiceImport, Namespace: testNamespace}
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, newServiceImportLookupKey, createdServiceImport); err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+
+			By("By checking mcs condition and expecting unknown")
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, mcsLookupKey, createdMultiClusterService); err != nil {
+					return false
+				}
+				expected := metav1.Condition{
+					Type:   string(fleetnetv1alpha1.MultiClusterServiceValid),
+					Status: metav1.ConditionUnknown,
+					Reason: conditionReasonUnknownServiceImport,
+				}
+				return len(createdMultiClusterService.Status.Conditions) == 1 &&
+					cmp.Equal(createdMultiClusterService.Status.Conditions[0], expected, cmpopts.IgnoreFields(metav1.Condition{}, "Message", "LastTransitionTime", "ObservedGeneration"))
+			}, timeout, interval).Should(BeTrue())
+
+			By("By deleting mcs")
+			Expect(k8sClient.Delete(ctx, multiClusterService)).Should(Succeed())
+
+			By("By checking service import")
+			Eventually(func() (int, error) {
+				serviceImportList := &fleetnetv1alpha1.ServiceImportList{}
+				if err := k8sClient.List(ctx, serviceImportList, &client.ListOptions{Namespace: testNamespace}); err != nil {
+					return -1, err
+				}
+				return len(serviceImportList.Items), nil
+			}, duration, interval).Should(Equal(0))
+
+			By("By checking derived Service in the fleet-system")
+			Eventually(func() (int, error) {
+				serviceList := &corev1.ServiceList{}
+				if err := k8sClient.List(ctx, serviceList, &client.ListOptions{Namespace: systemNamespace}); err != nil {
+					return -1, err
+				}
+				return len(serviceList.Items), nil
+			}, duration, interval).Should(Equal(0))
+
+			By("By checking mcs")
+			Eventually(func() bool {
+				return errors.IsNotFound(k8sClient.Get(ctx, mcsLookupKey, createdMultiClusterService))
+			}, timeout, interval).Should(BeTrue())
+		})
+	})
+})
---
+++ pkg/controllers/multiclusterservice/suite_test.go
@@ -0,0 +1,103 @@
+package multiclusterservice
+
+import (
+	"context"
+	"flag"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	// +kubebuilder:scaffold:imports
+
+	fleetnetv1alpha1 "go.goms.io/fleet-networking/api/v1alpha1"
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var (
+	cfg       *rest.Config
+	mgr       manager.Manager
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	ctx       context.Context
+	cancel    context.CancelFunc
+)
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "MultiClusterService Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+	klog.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	ctx, cancel = context.WithCancel(context.TODO())
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("../../../", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	err = fleetnetv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	//+kubebuilder:scaffold:scheme
+	By("construct the k8s client")
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	By("starting the controller manager")
+	klog.InitFlags(flag.CommandLine)
+	flag.Parse()
+
+	mgr, err = ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:             scheme.Scheme,
+		MetricsBindAddress: "0",
+		Logger:             klogr.NewWithOptions(klogr.WithFormat(klogr.FormatKlog)),
+		Port:               4848,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	err = (&Reconciler{
+		Client:               mgr.GetClient(),
+		Scheme:               mgr.GetScheme(),
+		FleetSystemNamespace: "fleet-system",
+	}).SetupWithManager(mgr)
+	Expect(err).ToNot(HaveOccurred())
+
+	ctx, cancel = context.WithCancel(context.TODO())
+	go func() {
+		defer GinkgoRecover()
+		err = mgr.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
+})
+
+var _ = AfterSuite(func() {
+	defer klog.Flush()
+
+	cancel()
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
