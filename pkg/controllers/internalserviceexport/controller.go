a84d2ba9af7041e4753088de9c84df15801c44f6
michaelawyu <chenyu1@microsoft.com>
2022-07-05 14:22:02 +0800

    feat: add InternalServiceExport controller (#31)

---
+++ pkg/controllers/internalserviceexport/controller.go
@@ -0,0 +1,123 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+// package internalserviceexport features the InternalServiceExport controller for reporting back conflict resolution
+// status from the fleet to a member cluster.
+package internalserviceexport
+
+import (
+	"context"
+	"reflect"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	fleetnetv1alpha1 "go.goms.io/fleet-networking/api/v1alpha1"
+)
+
+// Reconciler reconciles the update of an InternalServiceExport.
+type Reconciler struct {
+	memberClient client.Client
+	hubClient    client.Client
+}
+
+//+kubebuilder:rbac:groups=networking.fleet.azure.com,resources=internalserviceexports,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.fleet.azure.com,resources=internalserviceexports/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=networking.fleet.azure.com,resources=serviceexports,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.fleet.azure.com,resources=serviceexports/status,verbs=get;update;patch
+
+// Reconcile reports back whether an export of a Service has been accepted with no conflict detected.
+func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	internalSvcExportRef := klog.KRef(req.Namespace, req.Name)
+	startTime := time.Now()
+	klog.V(2).InfoS("Reconciliation starts", "internalServiceExport", internalSvcExportRef)
+	defer func() {
+		latency := time.Since(startTime).Seconds()
+		klog.V(2).InfoS("Reconciliation ends", "internalServiceExport", internalSvcExportRef, "latency", latency)
+	}()
+
+	// Retrieve the InternalServiceExport object.
+	var internalSvcExport fleetnetv1alpha1.InternalServiceExport
+	if err := r.hubClient.Get(ctx, req.NamespacedName, &internalSvcExport); err != nil {
+		klog.ErrorS(err, "Failed to get internal svc export", "internalServiceExport", internalSvcExportRef)
+		// Skip the reconciliation if the InternalServiceExport does not exist.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Check if the exported Service exists.
+	svcNS := internalSvcExport.Spec.ServiceReference.Namespace
+	svcName := internalSvcExport.Spec.ServiceReference.Name
+	svcExportRef := klog.KRef(svcNS, svcName)
+	var svcExport fleetnetv1alpha1.ServiceExport
+	err := r.memberClient.Get(ctx, types.NamespacedName{Namespace: svcNS, Name: svcName}, &svcExport)
+	switch {
+	case errors.IsNotFound(err):
+		// The absence of ServiceExport suggests that the Service should not be, yet has been, exported. Normally
+		// this situation will never happen as the ServiceExport controller guarantees, using the cleanup finalizer,
+		// that a ServiceExport will only be deleted after the Service has been unexported. In some corner cases,
+		// however, e.g. the user chooses to remove the finalizer explicitly, a Service can be left over in the hub
+		// cluster, and it is up to this controller to remove it.
+		klog.V(2).InfoS("Svc export does not exist; delete the internal svc export",
+			"serviceExport", svcExportRef,
+			"internalServiceExport", internalSvcExportRef,
+		)
+		if err := r.hubClient.Delete(ctx, &internalSvcExport); err != nil {
+			klog.ErrorS(err, "Failed to delete internal svc export", "internalServiceExport", internalSvcExportRef)
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	case err != nil:
+		// An unexpected error occurs.
+		klog.ErrorS(err, "Failed to get svc export", "serviceExport", svcExportRef)
+		return ctrl.Result{}, err
+	}
+
+	// Report back conflict resolution result.
+	klog.V(2).InfoS("Report back conflict resolution result", "internalServiceExport", internalSvcExportRef)
+	if err := r.reportBackConflictCondition(ctx, &svcExport, &internalSvcExport); err != nil {
+		klog.ErrorS(err, "Failed to report back conflict resolution result", "serviceExport", svcExportRef)
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager builds a controller with InternalSvcExportReconciler and sets it up with a
+// (multi-namespaced) controller manager.
+func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).For(&fleetnetv1alpha1.InternalServiceExport{}).Complete(r)
+}
+
+// reportBackConflictCond reports the ServiceExportConflict condition added to the InternalServiceExport object in the
+// hub cluster back to the ServiceExport ojbect in the member cluster.
+func (r *Reconciler) reportBackConflictCondition(ctx context.Context,
+	svcExport *fleetnetv1alpha1.ServiceExport,
+	internalSvcExport *fleetnetv1alpha1.InternalServiceExport) error {
+	internalSvcExportRef := klog.KRef(internalSvcExport.Namespace, internalSvcExport.Name)
+	internalSvcExportConflictCond := meta.FindStatusCondition(internalSvcExport.Status.Conditions,
+		string(fleetnetv1alpha1.ServiceExportConflict))
+	if internalSvcExportConflictCond == nil {
+		// No conflict condition to report back; this is the expected behavior when the conflict resolution process
+		// has not completed yet.
+		klog.V(4).InfoS("No conflict condition to report back", "internalServiceExport", internalSvcExportRef)
+		return nil
+	}
+
+	svcExportConflictCond := meta.FindStatusCondition(svcExport.Status.Conditions, string(fleetnetv1alpha1.ServiceExportConflict))
+	if reflect.DeepEqual(internalSvcExportConflictCond, svcExportConflictCond) {
+		// The conflict condition has not changed and there is no need to report back; this is also an expected
+		// behavior.
+		klog.V(4).InfoS("No update on the conflict condition", "internalServiceExport", internalSvcExportRef)
+		return nil
+	}
+
+	// Update the conditions
+	meta.SetStatusCondition(&svcExport.Status.Conditions, *internalSvcExportConflictCond)
+	return r.memberClient.Status().Update(ctx, svcExport)
+}
---
+++ pkg/controllers/internalserviceexport/controller_test.go
@@ -0,0 +1,199 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package internalserviceexport
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	fleetnetv1alpha1 "go.goms.io/fleet-networking/api/v1alpha1"
+)
+
+const (
+	hubNSForMember = "bravelion"
+	memberUserNS   = "work"
+	svcName        = "app"
+)
+
+// ignoredCondFields are fields that should be ignored when comparing conditions.
+var ignoredCondFields = cmpopts.IgnoreFields(metav1.Condition{}, "ObservedGeneration", "LastTransitionTime")
+
+// conflictedServiceExportConflictCondition returns a ServiceExportConflict condition that reports an export conflict.
+func conflictedServiceExportConflictCondition(svcNamespace string, svcName string) metav1.Condition {
+	return metav1.Condition{
+		Type:               string(fleetnetv1alpha1.ServiceExportConflict),
+		Status:             metav1.ConditionTrue,
+		ObservedGeneration: 1,
+		LastTransitionTime: metav1.Now(),
+		Reason:             "ConflictFound",
+		Message:            fmt.Sprintf("service %s/%s is in conflict with other exported services", svcNamespace, svcName),
+	}
+}
+
+// unconflictedServiceExportConflictCondition returns a ServiceExportConflict condition that reports a successful
+// export (no conflict).
+func unconflictedServiceExportConflictCondition(svcNamespace string, svcName string) metav1.Condition {
+	return metav1.Condition{
+		Type:               string(fleetnetv1alpha1.ServiceExportConflict),
+		Status:             metav1.ConditionFalse,
+		ObservedGeneration: 2,
+		LastTransitionTime: metav1.Now(),
+		Reason:             "NoConflictFound",
+		Message:            fmt.Sprintf("service %s/%s is exported without conflict", svcNamespace, svcName),
+	}
+}
+
+// unknownServiceExportConflictCondition returns a ServiceExportConflict condition that reports an in-progress
+// conflict resolution session.
+func unknownServiceExportConflictCondition(svcNamespace string, svcName string) metav1.Condition {
+	return metav1.Condition{
+		Type:               string(fleetnetv1alpha1.ServiceExportConflict),
+		Status:             metav1.ConditionUnknown,
+		ObservedGeneration: 0,
+		LastTransitionTime: metav1.Now(),
+		Reason:             "PendingConflictResolution",
+		Message:            fmt.Sprintf("service %s/%s is pending export conflict resolution", svcNamespace, svcName),
+	}
+}
+
+// Bootstrap the test environment.
+func TestMain(m *testing.M) {
+	// Add custom APIs to the runtime scheme
+	err := fleetnetv1alpha1.AddToScheme(scheme.Scheme)
+	if err != nil {
+		log.Fatalf("failed to add custom APIs to the runtime scheme: %v", err)
+	}
+
+	os.Exit(m.Run())
+}
+
+// TestReportBackConflictCondition tests the *Reconciler.reportBackConflictCondition method.
+func TestReportBackConflictCondition(t *testing.T) {
+	testCases := []struct {
+		name              string
+		svcExport         *fleetnetv1alpha1.ServiceExport
+		internalSvcExport *fleetnetv1alpha1.InternalServiceExport
+		expectedConds     []metav1.Condition
+	}{
+		{
+			name: "should not report back conflict cond (no condition yet)",
+			svcExport: &fleetnetv1alpha1.ServiceExport{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: memberUserNS,
+					Name:      svcName,
+				},
+				Status: fleetnetv1alpha1.ServiceExportStatus{
+					Conditions: []metav1.Condition{
+						unknownServiceExportConflictCondition(memberUserNS, svcName),
+					},
+				},
+			},
+			internalSvcExport: &fleetnetv1alpha1.InternalServiceExport{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: hubNSForMember,
+					Name:      fmt.Sprintf("%s-%s", memberUserNS, svcName),
+				},
+			},
+			expectedConds: []metav1.Condition{
+				unknownServiceExportConflictCondition(memberUserNS, svcName),
+			},
+		},
+		{
+			name: "should not report back conflict cond (no update)",
+			svcExport: &fleetnetv1alpha1.ServiceExport{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: memberUserNS,
+					Name:      svcName,
+				},
+				Status: fleetnetv1alpha1.ServiceExportStatus{
+					Conditions: []metav1.Condition{
+						unconflictedServiceExportConflictCondition(memberUserNS, svcName),
+					},
+				},
+			},
+			internalSvcExport: &fleetnetv1alpha1.InternalServiceExport{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: hubNSForMember,
+					Name:      fmt.Sprintf("%s-%s", memberUserNS, svcName),
+				},
+				Status: fleetnetv1alpha1.InternalServiceExportStatus{
+					Conditions: []metav1.Condition{
+						unconflictedServiceExportConflictCondition(memberUserNS, svcName),
+					},
+				},
+			},
+			expectedConds: []metav1.Condition{
+				unconflictedServiceExportConflictCondition(memberUserNS, svcName),
+			},
+		},
+		{
+			name: "should report back conflict cond",
+			svcExport: &fleetnetv1alpha1.ServiceExport{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: memberUserNS,
+					Name:      svcName,
+				},
+				Status: fleetnetv1alpha1.ServiceExportStatus{
+					Conditions: []metav1.Condition{},
+				},
+			},
+			internalSvcExport: &fleetnetv1alpha1.InternalServiceExport{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: hubNSForMember,
+					Name:      fmt.Sprintf("%s-%s", memberUserNS, svcName),
+				},
+				Status: fleetnetv1alpha1.InternalServiceExportStatus{
+					Conditions: []metav1.Condition{
+						conflictedServiceExportConflictCondition(memberUserNS, svcName),
+					},
+				},
+			},
+			expectedConds: []metav1.Condition{
+				conflictedServiceExportConflictCondition(memberUserNS, svcName),
+			},
+		},
+	}
+
+	ctx := context.Background()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			fakeMemberClient := fake.NewClientBuilder().
+				WithScheme(scheme.Scheme).
+				WithObjects(tc.svcExport).
+				Build()
+			fakeHubClient := fake.NewClientBuilder().Build()
+			reconciler := Reconciler{
+				memberClient: fakeMemberClient,
+				hubClient:    fakeHubClient,
+			}
+
+			if err := reconciler.reportBackConflictCondition(ctx, tc.svcExport, tc.internalSvcExport); err != nil {
+				t.Fatalf("failed to report back conflict cond: %v", err)
+			}
+
+			var updatedSvcExport = &fleetnetv1alpha1.ServiceExport{}
+			updatedSvcExportKey := types.NamespacedName{Namespace: tc.svcExport.Namespace, Name: tc.svcExport.Name}
+			if err := fakeMemberClient.Get(ctx, updatedSvcExportKey, updatedSvcExport); err != nil {
+				t.Fatalf("failed to get updated svc export: %v", err)
+			}
+			conds := updatedSvcExport.Status.Conditions
+			if !cmp.Equal(conds, tc.expectedConds, ignoredCondFields) {
+				t.Fatalf("conds are not correctly updated, got %+v, want %+v", conds, tc.expectedConds)
+			}
+		})
+	}
+}
