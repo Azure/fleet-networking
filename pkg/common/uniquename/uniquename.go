ada3a86d1e0769665c3c60357bceb1becbf99ff2
michaelawyu <chenyu1@microsoft.com>
2022-07-08 13:50:57 +0800

    feat: add unique name helper funcs (#37)

---
+++ pkg/common/uniquename/uniquename.go
@@ -0,0 +1,207 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+// Package uniquename features utility functions that help format unique names for exporting and importing
+// cluster-scoped and fleet-scoped resources.
+package uniquename
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"unicode"
+
+	"k8s.io/apimachinery/pkg/util/uuid"
+	"k8s.io/apimachinery/pkg/util/validation"
+)
+
+type Format int
+
+const (
+	// DNS1123Subdomain dictates that the unique name should be a valid RFC 1123 DNS subdomain.
+	DNS1123Subdomain Format = 1
+	// DNS1123Label dictates that the unique name should be a valid RFC 1123 DNS label.
+	DNS1123Label Format = 2
+	// DNS1035Label dictates that the unique name should be a valid RFC 1035 DNS label.
+	DNS1035Label Format = 3
+
+	uuidLength = 5
+)
+
+// minInt returns the smaller one of two integers.
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// startsWithNumericCharacter returns if a string starts with a numeric character.
+func startsWithNumericCharacter(s string) bool {
+	return unicode.IsDigit(rune(s[0]))
+}
+
+// removeDots removes all dot (".") occurrences in a string.
+func removeDots(s string) string {
+	return strings.ReplaceAll(s, ".", "")
+}
+
+// ClusterScopedUniqueName returns a name that is guaranteed to be unique within a cluster.
+// The name is formatted using an object's namespace, its name, and a 5 character long UUID suffix; the format
+// is [NAMESPACE]-[NAME]-[SUFFIX], e.g. an object `app` from the namespace `work` will be assigned a unique
+// name like `work-app-1x2yz`. The function may truncate name components as it sees fit.
+// Note: this function assumes that
+// * the input object namespace is a valid RFC 1123 DNS label; and
+// * the input object name follows one of the three formats used in Kubernetes (RFC 1123 DNS subdomain,
+// 	 RFC 1123 DNS label, RFC 1035 DNS label).
+func ClusterScopedUniqueName(format Format, namespace, name string) (string, error) {
+	reservedSlots := 2 + uuidLength // 2 dashes + 5 character UUID string
+
+	switch format {
+	case DNS1123Subdomain:
+		availableSlots := validation.DNS1123SubdomainMaxLength // 253 characters
+		slotsPerSeg := (availableSlots - reservedSlots) / 2
+		uniqueName := fmt.Sprintf("%s-%s-%s",
+			namespace[:minInt(slotsPerSeg, len(namespace))],
+			name[:minInt(slotsPerSeg, len(name))],
+			uuid.NewUUID()[:uuidLength],
+		)
+
+		if errs := validation.IsDNS1123Subdomain(uniqueName); len(errs) != 0 {
+			return "", fmt.Errorf("failed to format a unique RFC 1123 DNS subdomain name with namespace %s, name %s: %v", namespace, name, errs)
+		}
+		return uniqueName, nil
+	case DNS1123Label:
+		availableSlots := validation.DNS1123LabelMaxLength // 63 characters
+		slotsPerSeg := (availableSlots - reservedSlots) / 2
+
+		// If the object name is a RFC 1123 DNS subdomain, it may include dot characters, which is not allowed in
+		// RFC 1123 DNS labels.
+		name = removeDots(name)
+
+		uniqueName := fmt.Sprintf("%s-%s-%s",
+			namespace[:minInt(slotsPerSeg, len(namespace))],
+			name[:minInt(slotsPerSeg, len(name))],
+			uuid.NewUUID()[:uuidLength],
+		)
+
+		if errs := validation.IsDNS1123Label(uniqueName); len(errs) != 0 {
+			return "", fmt.Errorf("failed to format a unique RFC 1123 DNS label name with namespace %s, name %s: %v", namespace, name, errs)
+		}
+		return uniqueName, nil
+	case DNS1035Label:
+		availableSlots := validation.DNS1035LabelMaxLength // 63 characters
+		slotsPerSeg := (availableSlots - reservedSlots) / 2
+
+		// Namespace names are RFC 1123 DNS labels, which may start with an alphanumeric character; RFC 1035 DNS
+		// labels, on the other hand, does not allow numeric characters at the beginning of the string.
+		if startsWithNumericCharacter(namespace) {
+			namespace = "ns" + namespace
+		}
+		// If the object name is a RFC 1123 DNS subdomain, it may include dot characters, which is not allowed in
+		// RFC 1035 DNS labels.
+		name = removeDots(name)
+
+		uniqueName := fmt.Sprintf("%s-%s-%s",
+			namespace[:minInt(slotsPerSeg, len(namespace))],
+			name[:minInt(slotsPerSeg, len(name))],
+			uuid.NewUUID()[:uuidLength],
+		)
+
+		if errs := validation.IsDNS1035Label(uniqueName); len(errs) != 0 {
+			return "", fmt.Errorf("failed to format a unique RFC 1035 DNS label name with namespace %s, name %s: %v", namespace, name, errs)
+		}
+		return uniqueName, nil
+	}
+	return "", fmt.Errorf("not a valid name format: %d", format)
+}
+
+// FleetScopedUniqueName returns a name that is guaranteed to be unique within a cluster.
+// The name is formatted using an object's origin cluster, an object's namespace, its name, and a 5 character
+// long UUID suffix; the format is [CLUSTER ID]-[NAMESPACE]-[NAME]-[SUFFIX], e.g. an object `app` from the namespace
+// `work` in cluster `bravelion` will be assigned a unique name like `bravelion-work-app-1x2yz`. The function may
+// truncate name components as it sees fit.
+// Note: this function assumes that
+// * the input cluster ID is a valid RFC 1123 DNS subdomain; and
+// * the input object namespace is a valid RFC 1123 DNS label; and
+// * the input object name follows one of the three formats used in Kubernetes (RFC 1123 DNS subdomain,
+// 	 RFC 1123 DNS label, RFC 1035 DNS label).
+func FleetScopedUniqueName(format Format, clusterID, namespace, name string) (string, error) {
+	reservedSlots := 3 + uuidLength // 3 dashes + 5 character UUID string
+
+	switch format {
+	case DNS1123Subdomain:
+		availableSlots := validation.DNS1123SubdomainMaxLength // 253 characters
+		slotsPerSeg := (availableSlots - reservedSlots) / 3
+		uniqueName := fmt.Sprintf("%s-%s-%s-%s",
+			clusterID[:minInt(slotsPerSeg, len(clusterID))],
+			namespace[:minInt(slotsPerSeg, len(namespace))],
+			name[:minInt(slotsPerSeg, len(name))],
+			uuid.NewUUID()[:uuidLength],
+		)
+
+		if errs := validation.IsDNS1123Subdomain(uniqueName); len(errs) != 0 {
+			return "", fmt.Errorf("failed to format a unique RFC 1123 DNS subdomain name with cluster ID %s, namespace %s, name %s: %v",
+				clusterID, namespace, name, errs)
+		}
+		return uniqueName, nil
+	case DNS1123Label:
+		availableSlots := validation.DNS1123LabelMaxLength // 63 characters
+		slotsPerSeg := (availableSlots - reservedSlots) / 3
+
+		// If the cluster ID and object name are valid RFC 1123 DNS subdomains, they may include dot characters,
+		// which is not allowed in RFC 1123 DNS labels.
+		clusterID = removeDots(clusterID)
+		name = removeDots(name)
+
+		uniqueName := fmt.Sprintf("%s-%s-%s-%s",
+			clusterID[:minInt(slotsPerSeg, len(clusterID))],
+			namespace[:minInt(slotsPerSeg, len(namespace))],
+			name[:minInt(slotsPerSeg, len(name))],
+			uuid.NewUUID()[:uuidLength],
+		)
+
+		if errs := validation.IsDNS1123Label(uniqueName); len(errs) != 0 {
+			return "", fmt.Errorf("failed to format a unique RFC 1123 DNS label name with cluster ID %s, namespace %s, name %s: %v",
+				clusterID, namespace, name, errs)
+		}
+		return uniqueName, nil
+	case DNS1035Label:
+		availableSlots := validation.DNS1035LabelMaxLength // 63 characters
+		slotsPerSeg := (availableSlots - reservedSlots) / 3
+
+		// If the cluster ID and object name are valid RFC 1123 DNS subdomains, they may include dot characters,
+		// which is not allowed in RFC 1123 DNS labels.
+		clusterID = removeDots(clusterID)
+		name = removeDots(name)
+
+		uniqueName := fmt.Sprintf("%s-%s-%s-%s",
+			clusterID[:minInt(slotsPerSeg, len(clusterID))],
+			namespace[:minInt(slotsPerSeg, len(namespace))],
+			name[:minInt(slotsPerSeg, len(name))],
+			uuid.NewUUID()[:uuidLength],
+		)
+
+		if errs := validation.IsDNS1035Label(uniqueName); len(errs) != 0 {
+			return "", fmt.Errorf("failed to format a unique RFC 1035 DNS label name with cluster ID %s, namespace %s, name %s: %v",
+				clusterID, namespace, name, errs)
+		}
+		return uniqueName, nil
+	}
+	return "", fmt.Errorf("not a valid name format: %d", format)
+}
+
+// RandomLowerCaseAlphabeticString returns a string of lower case alphabetic characters only. This function
+// is best used for fallback cases where one cannot format a unique name as expected, as a lower case
+// alphabetic string of proper length is always a valid Kubernetes object name, regardless of the required name
+// format for the object (RFC 1123 DNS subdomain, RFC 1123 DNS label, or RFC 1035 DNS label).
+func RandomLowerCaseAlphabeticString(n int) string {
+	alphabet := []rune("abcdefghijklmnopqrstuvwxyz")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))] //nolint:gosec
+	}
+	return string(b)
+}
---
+++ pkg/common/uniquename/uniquename_test.go
@@ -0,0 +1,280 @@
+package uniquename
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+)
+
+const (
+	clusterID                   = "bravelion"
+	objectNS                    = "work"
+	objectName                  = "app"
+	expectedClusterScopedPrefix = "work-app-"
+	expectedClusterScopedLength = 14
+	expectedFleetScopedPrefix   = "bravelion-work-app-"
+	expectedFleetScopedLength   = 24
+
+	longClusterID  = "lhwh0nrw3d03no2f1rf53dl9gliwe3rneqeuzt6k2qa9qn6epm"           // 50 characters long
+	longObjectNS   = "mdlmpqe2ev31zgxar1gswscd3hsvypl5leh4nolfnq0vdg56a7a08iiu4988" // 60 characters long
+	longObjectName = "c7t2c6oppjnryqcihwweexeobs7tlmf08ha4qb5htc4cifzpalhb5ec2lbh3" +
+		"j73reciaz2f0jfd2rl5qba6rzuuwgyw6d9e6la19bo89k41lphln4s4dy1gr" +
+		"h1dvua17iu4ro61dxo91ayovns8cgnmshlsflmi68e3najm7dw5dqe17pih7" +
+		"up0dtyvrqxyp90sxedbf" // 200 characters long
+)
+
+// TestClusterScopedUniqueName tests the ClusterScopedUniqueName function.
+func TestClusterScopedUniqueName(t *testing.T) {
+	testCases := []struct {
+		name       string
+		format     Format
+		objectNS   string
+		objectName string
+		wantPrefix string
+		wantLength int
+	}{
+		{
+			name:       "should format RFC 1123 DNS subdomain name",
+			format:     DNS1123Subdomain,
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedClusterScopedPrefix,
+			wantLength: expectedClusterScopedLength,
+		},
+		{
+			name:       "should format RFC 1123 DNS subdomain name (truncated)",
+			format:     DNS1123Subdomain,
+			objectNS:   longObjectNS,
+			objectName: longObjectName,
+			wantPrefix: "mdlmpqe2ev31zgxar1gswscd3hsvypl5leh4nolfnq0vdg56a7a08iiu4988" +
+				"-c7t2c6oppjnryqcihwweexeobs7tlmf08ha4qb5htc4cifzpalhb5ec2lbh" +
+				"3j73reciaz2f0jfd2rl5qba6rzuuwgyw6d9e6la19bo89k41lphln4s4dy1g" +
+				"rh1d-", // 184 characters long
+			wantLength: 190,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name",
+			format:     DNS1123Label,
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedClusterScopedPrefix,
+			wantLength: expectedClusterScopedLength,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name (no dots allowed)",
+			format:     DNS1123Label,
+			objectNS:   objectNS,
+			objectName: objectName + ".",
+			wantPrefix: expectedClusterScopedPrefix,
+			wantLength: expectedClusterScopedLength,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name (truncated)",
+			format:     DNS1123Label,
+			objectNS:   longObjectNS,
+			objectName: longObjectName,
+			wantPrefix: "mdlmpqe2ev31zgxar1gswscd3hsv-c7t2c6oppjnryqcihwweexeobs7t-",
+			wantLength: 63,
+		},
+		{
+			name:       "should format RFC 1035 DNS label name",
+			format:     DNS1035Label,
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedClusterScopedPrefix,
+			wantLength: expectedClusterScopedLength,
+		},
+		{
+			name:       "should format RFC 1035 DNS label name (no dots allowed)",
+			format:     DNS1035Label,
+			objectNS:   objectNS,
+			objectName: objectName + ".",
+			wantPrefix: expectedClusterScopedPrefix,
+			wantLength: expectedClusterScopedLength,
+		},
+		{
+			name:       "should format RFC 1035 DNS label name (no numeric starts allowed)",
+			format:     DNS1035Label,
+			objectNS:   "0" + objectNS,
+			objectName: objectName,
+			wantPrefix: "ns0" + expectedClusterScopedPrefix,
+			wantLength: expectedClusterScopedLength + 3,
+		},
+		{
+			name:       "should format RFC 1035 DNS label name (truncated)",
+			format:     DNS1035Label,
+			objectNS:   longObjectNS,
+			objectName: longObjectName,
+			wantPrefix: "mdlmpqe2ev31zgxar1gswscd3hsv-c7t2c6oppjnryqcihwweexeobs7t-",
+			wantLength: 63,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			uniqueName, err := ClusterScopedUniqueName(tc.format, tc.objectNS, tc.objectName)
+			if err != nil {
+				t.Fatalf("ClusterScopedUniqueName(%d, %s, %s), got %v, want no error", tc.format, tc.objectNS, tc.objectName, err)
+			}
+			if !strings.HasPrefix(uniqueName, tc.wantPrefix) {
+				t.Errorf("ClusterScopedUniqueName(%d, %s, %s)=%s, want prefix %s",
+					tc.format, tc.objectNS, tc.objectName, uniqueName, tc.wantPrefix)
+			}
+			if len(uniqueName) != tc.wantLength {
+				t.Errorf("ClusterScopedUniqueName(%d, %s, %s)=%s, got length %d, want length %d",
+					tc.format, tc.objectNS, tc.objectName, uniqueName, len(uniqueName), tc.wantLength)
+			}
+		})
+	}
+}
+
+// TestFleetScopedUniqueName tests the FleetScopedUniqueName function.
+func TestFleetScopedUniqueName(t *testing.T) {
+	testCases := []struct {
+		name       string
+		format     Format
+		clusterID  string
+		objectNS   string
+		objectName string
+		wantPrefix string
+		wantLength int
+	}{
+		{
+			name:       "should format RFC 1123 DNS subdomain name",
+			format:     DNS1123Subdomain,
+			clusterID:  clusterID,
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedFleetScopedPrefix,
+			wantLength: expectedFleetScopedLength,
+		},
+		{
+			name:       "should format RFC 1123 DNS subdomain name (truncated)",
+			format:     DNS1123Subdomain,
+			clusterID:  longClusterID,
+			objectNS:   longObjectNS,
+			objectName: longObjectName,
+			wantPrefix: "lhwh0nrw3d03no2f1rf53dl9gliwe3rneqeuzt6k2qa9qn6epm-mdlmpqe2e" +
+				"v31zgxar1gswscd3hsvypl5leh4nolfnq0vdg56a7a08iiu4988-c7t2c6op" +
+				"pjnryqcihwweexeobs7tlmf08ha4qb5htc4cifzpalhb5ec2lbh3j73recia" +
+				"z2f0jfd2rl5qb-",
+			wantLength: 199,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name",
+			format:     DNS1123Label,
+			clusterID:  clusterID,
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedFleetScopedPrefix,
+			wantLength: expectedFleetScopedLength,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name (truncated)",
+			format:     DNS1123Label,
+			clusterID:  longClusterID,
+			objectNS:   longObjectNS,
+			objectName: longObjectName,
+			wantPrefix: "lhwh0nrw3d03no2f1r-mdlmpqe2ev31zgxar1-c7t2c6oppjnryqcihw-",
+			wantLength: 62,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name (no dots allowed in cluster ID)",
+			format:     DNS1123Label,
+			clusterID:  clusterID + ".",
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedFleetScopedPrefix,
+			wantLength: expectedFleetScopedLength,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name (not dots allowed in name)",
+			format:     DNS1123Label,
+			clusterID:  clusterID,
+			objectNS:   objectNS,
+			objectName: objectName + ".",
+			wantPrefix: expectedFleetScopedPrefix,
+			wantLength: expectedFleetScopedLength,
+		},
+		{
+			name:       "should format RFC 1035 DNS label name",
+			format:     DNS1035Label,
+			clusterID:  clusterID,
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedFleetScopedPrefix,
+			wantLength: expectedFleetScopedLength,
+		},
+		{
+			name:       "should format RFC 1035 DNS label name (truncated)",
+			format:     DNS1035Label,
+			clusterID:  longClusterID,
+			objectNS:   longObjectNS,
+			objectName: longObjectName,
+			wantPrefix: "lhwh0nrw3d03no2f1r-mdlmpqe2ev31zgxar1-c7t2c6oppjnryqcihw-",
+			wantLength: 62,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name (no dots allowed in cluster ID)",
+			format:     DNS1035Label,
+			clusterID:  clusterID + ".",
+			objectNS:   objectNS,
+			objectName: objectName,
+			wantPrefix: expectedFleetScopedPrefix,
+			wantLength: expectedFleetScopedLength,
+		},
+		{
+			name:       "should format RFC 1123 DNS label name (not dots allowed in name)",
+			format:     DNS1035Label,
+			clusterID:  clusterID,
+			objectNS:   objectNS,
+			objectName: objectName + ".",
+			wantPrefix: expectedFleetScopedPrefix,
+			wantLength: expectedFleetScopedLength,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			uniqueName, err := FleetScopedUniqueName(tc.format, tc.clusterID, tc.objectNS, tc.objectName)
+			if err != nil {
+				t.Fatalf("FleetScopedUniqueName(%d, %s, %s, %s), got %v, want no error", tc.format, tc.clusterID, tc.objectNS, tc.objectName, err)
+			}
+			if !strings.HasPrefix(uniqueName, tc.wantPrefix) {
+				t.Errorf("FleetScopedUniqueName(%d, %s, %s, %s)=%s, want prefix %s",
+					tc.format, tc.clusterID, tc.objectNS, tc.objectName, uniqueName, tc.wantPrefix)
+			}
+			if len(uniqueName) != tc.wantLength {
+				t.Errorf("FleetScopedUniqueName(%d, %s, %s, %s)=%s, got length %d, want length %d",
+					tc.format, tc.clusterID, tc.objectNS, tc.objectName, uniqueName, len(uniqueName), tc.wantLength)
+			}
+		})
+	}
+}
+
+// TestRandomLowerCaseAlphabeticString tests the RandomLowerCaseAlphabeticString function.
+func TestRandomLowerCaseAlphabeticString(t *testing.T) {
+	testCases := []struct {
+		name   string
+		length int
+	}{
+		{
+			name:   "should return lower case alphabetic string",
+			length: 20,
+		},
+	}
+
+	var isAlphabeticString = regexp.MustCompile(`^[a-z]+$`).MatchString
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			s := RandomLowerCaseAlphabeticString(tc.length)
+			if len(s) != tc.length {
+				t.Errorf("RandomLowerCaseAlphabeticString(%d)=%s, got length %d, want length %d", tc.length, s, len(s), tc.length)
+			}
+			if !isAlphabeticString(s) {
+				t.Errorf("RandomLowerCaseAlphabeticString(%d)=%s, want lower case alphabetic characters only", tc.length, s)
+			}
+		})
+	}
+}
