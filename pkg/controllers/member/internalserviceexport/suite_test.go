7a18495e8cc52ceb67676b238aca08e46bdafe35
michaelawyu <chenyu1@microsoft.com>
2022-07-11 15:48:26 +0800

    Rebased + Minor fixes

--- pkg/controllers/member/internalserviceexport/suite_test.go
+++ pkg/controllers/member/internalserviceexport/suite_test.go
@@ -15,6 +15,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -113,6 +114,7 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
+	defer klog.Flush()
 	cancel()
 
 	By("tearing down the test environment")
