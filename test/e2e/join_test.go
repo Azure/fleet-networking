5c67b03eea8b02cceb2e7eec9bbc57832219853c
Zhiying Lin <54013513+zhiying-lin@users.noreply.github.com>
2022-09-06 14:11:57 +0800

    test: add more join/leave tests (#105)

--- test/e2e/join_test.go
+++ test/e2e/join_test.go
@@ -18,16 +18,21 @@ import (
 
 	fleetv1alpha1 "go.goms.io/fleet/apis/v1alpha1"
 
+	fleetnetv1alpha1 "go.goms.io/fleet-networking/api/v1alpha1"
 	"go.goms.io/fleet-networking/test/e2e/framework"
 )
 
-var _ = Describe("Test Join/Unjoin workflow", func() {
+// Serial - Ginkgo will guarantee that these specs will never run in parallel with other specs.
+// Ordered - Ginkgo will guarantee that specs in an Ordered container will run sequentially, in the order they are written.
+// When member cluster leaves, it will delete all the networking resources and cause the flaky behaviors.
+var _ = Describe("Test Join/Leave workflow", Serial, Ordered, func() {
 	var (
 		ctx                    context.Context
 		memberClusterName      = memberClusterNames[0]
 		memberClusterNamespace = "fleet-member-" + memberClusterName
 		imcKey                 = types.NamespacedName{Namespace: memberClusterNamespace, Name: memberClusterName}
 		imc                    fleetv1alpha1.InternalMemberCluster
+		memberCluster          *framework.Cluster
 		cmpOptions             = []cmp.Option{
 			cmpopts.IgnoreFields(fleetv1alpha1.AgentStatus{}, "LastReceivedHeartbeat"),
 			cmpopts.IgnoreFields(metav1.Condition{}, "LastTransitionTime", "ObservedGeneration"),
@@ -38,7 +43,7 @@ var _ = Describe("Test Join/Unjoin workflow", func() {
 		heartbeatPeriod = 2
 	)
 
-	Context("Member cluster agents should join fleet", func() {
+	Context("Member cluster agents should join/leave fleet", func() {
 		BeforeEach(func() {
 			By("Creating internalMemberCluster")
 			ctx = context.Background()
@@ -53,6 +58,7 @@ var _ = Describe("Test Join/Unjoin workflow", func() {
 				},
 			}
 			Expect(hubCluster.Client().Create(ctx, &imc)).Should(Succeed(), "Failed to create internalMemberCluster %s", memberClusterName)
+			memberCluster = memberClusters[0]
 		})
 
 		AfterEach(func() {
@@ -97,6 +103,214 @@ var _ = Describe("Test Join/Unjoin workflow", func() {
 			Expect(imc.Status.AgentStatus[1].LastReceivedHeartbeat).ShouldNot(BeNil(), "heartbeat should not be nil")
 		})
 
-		// TODO add more test cases
+		It("Unjoin member cluster and should cleanup multiClusterService related resources", func() {
+			By("Creating multiClusterService")
+			serviceName := "my-svc"
+			mcs := fleetnetv1alpha1.MultiClusterService{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "my-mcs",
+					Namespace: testNamespace,
+				},
+				Spec: fleetnetv1alpha1.MultiClusterServiceSpec{
+					ServiceImport: fleetnetv1alpha1.ServiceImportRef{
+						Name: serviceName,
+					},
+				},
+			}
+			Expect(memberCluster.Client().Create(ctx, &mcs)).Should(Succeed())
+
+			By("Checking serviceImport in the member cluster")
+			Eventually(func() error {
+				serviceImport := fleetnetv1alpha1.ServiceImport{}
+				key := types.NamespacedName{Namespace: testNamespace, Name: serviceName}
+				return memberCluster.Client().Get(ctx, key, &serviceImport)
+			}, framework.PollTimeout, framework.PollInterval).Should(Succeed(), "Failed to get serviceImport created by the multiClusterService controller")
+
+			By("Updating internalMemberCluster spec to leave")
+			Eventually(func() error {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err
+				}
+				imc.Spec.State = fleetv1alpha1.ClusterStateLeave
+				return hubCluster.Client().Update(ctx, &imc)
+			}, framework.PollTimeout, framework.PollInterval).Should(Succeed(), "Failed to update internalMemberCluster spec")
+
+			By("Validating MultiClusterServiceAgent and ServiceExportImportAgent join status")
+			Eventually(func() string {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err.Error()
+				}
+				want := []fleetv1alpha1.AgentStatus{
+					{
+						Type: fleetv1alpha1.MultiClusterServiceAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionFalse,
+								Reason: "AgentLeft",
+							},
+						},
+					},
+					{
+						Type: fleetv1alpha1.ServiceExportImportAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionFalse,
+								Reason: "AgentLeft",
+							},
+						},
+					},
+				}
+				return cmp.Diff(want, imc.Status.AgentStatus, cmpOptions...)
+			}, framework.PollTimeout, framework.PollInterval).Should(BeEmpty(), "Validate internalMemberCluster mismatch (-want, +got):")
+
+			By("Validating multiClusterService resources")
+			mcsList := &fleetnetv1alpha1.MultiClusterServiceList{}
+			Expect(memberCluster.Client().List(ctx, mcsList)).Should(Succeed())
+			Expect(len(mcsList.Items) == 0).Should(BeTrue(), "Failed to cleanup multiClusterService resources")
+
+			By("Validating serviceImport resources in the member cluster")
+			serviceImportList := &fleetnetv1alpha1.ServiceImportList{}
+			Expect(memberCluster.Client().List(ctx, serviceImportList)).Should(Succeed())
+			Expect(len(serviceImportList.Items) == 0).Should(BeTrue(), "Failed to cleanup serviceImport resources")
+		})
+
+		It("Unjoin member cluster and should cleanup serviceExport related resources", func() {
+			By("Creating serviceExport")
+			serviceName := "my-svc"
+			serviceExport := fleetnetv1alpha1.ServiceExport{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceName,
+					Namespace: testNamespace,
+				},
+			}
+			Expect(memberCluster.Client().Create(ctx, &serviceExport)).Should(Succeed())
+
+			By("Updating internalMemberCluster spec to leave")
+			Eventually(func() error {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err
+				}
+				imc.Spec.State = fleetv1alpha1.ClusterStateLeave
+				return hubCluster.Client().Update(ctx, &imc)
+			}, framework.PollTimeout, framework.PollInterval).Should(Succeed(), "Failed to update internalMemberCluster spec")
+
+			By("Validating MultiClusterServiceAgent and ServiceExportImportAgent join status")
+			Eventually(func() string {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err.Error()
+				}
+				want := []fleetv1alpha1.AgentStatus{
+					{
+						Type: fleetv1alpha1.MultiClusterServiceAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionFalse,
+								Reason: "AgentLeft",
+							},
+						},
+					},
+					{
+						Type: fleetv1alpha1.ServiceExportImportAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionFalse,
+								Reason: "AgentLeft",
+							},
+						},
+					},
+				}
+				return cmp.Diff(want, imc.Status.AgentStatus, cmpOptions...)
+			}, framework.PollTimeout, framework.PollInterval).Should(BeEmpty(), "Validate internalMemberCluster mismatch (-want, +got):")
+
+			By("Validating serviceExport resources")
+			serviceExportList := &fleetnetv1alpha1.ServiceExportList{}
+			Expect(memberCluster.Client().List(ctx, serviceExportList)).Should(Succeed())
+			Expect(len(serviceExportList.Items) == 0).Should(BeTrue(), "Failed to cleanup serviceExport resources")
+		})
+
+		It("Unjoin member cluster first and then join", func() {
+			By("Updating internalMemberCluster spec to leave")
+			Eventually(func() error {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err
+				}
+				imc.Spec.State = fleetv1alpha1.ClusterStateLeave
+				return hubCluster.Client().Update(ctx, &imc)
+			}, framework.PollTimeout, framework.PollInterval).Should(Succeed(), "Failed to update internalMemberCluster spec")
+
+			By("Validating MultiClusterServiceAgent and ServiceExportImportAgent join status")
+			Eventually(func() string {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err.Error()
+				}
+				want := []fleetv1alpha1.AgentStatus{
+					{
+						Type: fleetv1alpha1.MultiClusterServiceAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionFalse,
+								Reason: "AgentLeft",
+							},
+						},
+					},
+					{
+						Type: fleetv1alpha1.ServiceExportImportAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionFalse,
+								Reason: "AgentLeft",
+							},
+						},
+					},
+				}
+				return cmp.Diff(want, imc.Status.AgentStatus, cmpOptions...)
+			}, framework.PollTimeout, framework.PollInterval).Should(BeEmpty(), "Validate internalMemberCluster mismatch (-want, +got):")
+
+			By("Updating internalMemberCluster spec to join")
+			Eventually(func() error {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err
+				}
+				imc.Spec.State = fleetv1alpha1.ClusterStateJoin
+				return hubCluster.Client().Update(ctx, &imc)
+			}, framework.PollTimeout, framework.PollInterval).Should(Succeed(), "Failed to update internalMemberCluster spec")
+
+			Eventually(func() string {
+				if err := hubCluster.Client().Get(ctx, imcKey, &imc); err != nil {
+					return err.Error()
+				}
+				want := []fleetv1alpha1.AgentStatus{
+					{
+						Type: fleetv1alpha1.MultiClusterServiceAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionTrue,
+								Reason: "AgentJoined",
+							},
+						},
+					},
+					{
+						Type: fleetv1alpha1.ServiceExportImportAgent,
+						Conditions: []metav1.Condition{
+							{
+								Type:   string(fleetv1alpha1.AgentJoined),
+								Status: metav1.ConditionTrue,
+								Reason: "AgentJoined",
+							},
+						},
+					},
+				}
+				return cmp.Diff(want, imc.Status.AgentStatus, cmpOptions...)
+			}, framework.PollTimeout, framework.PollInterval).Should(BeEmpty(), "Validate internalMemberCluster mismatch (-want, +got):")
+			Expect(imc.Status.AgentStatus[0].LastReceivedHeartbeat).ShouldNot(BeNil(), "heartbeat should not be nil")
+			Expect(imc.Status.AgentStatus[1].LastReceivedHeartbeat).ShouldNot(BeNil(), "heartbeat should not be nil")
+		})
 	})
 })
