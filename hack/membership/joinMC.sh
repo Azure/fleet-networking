b07f78ae73bdd4edbe4fe908fd319cae7450b379
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2024-08-06 18:12:34 -0700

    add validation to join script

--- hack/membership/joinMC.sh
+++ hack/membership/joinMC.sh
@@ -2,17 +2,41 @@
 # This can be achieved by running the script in fleet/hack/Azure/setup/joinMC.sh in the fleet repository https://github.com/Azure/fleet.
 # This script installs the fleet-networking member agents on each member cluster.
 
-export REFRESH_TOKEN_IMAGE_TAG="$1"
+if [ "$#" -lt 4 ]; then
+  echo "Usage: $0 <FLEET-IMAGE-TAG> <FLEET-NETWORKING-IMAGE-TAG> <HUB-CLUSTER-NAME> <MEMBER-CLUSTER-NAME-1> [<MEMBER-CLUSTER-NAME-2> ...]"
+  exit 1
+fi
+
+export FLEET_IMAGE_TAG="$1"
+if [[ $(curl "https://api.github.com/repos/Azure/fleet/tags") != *$1* ]] > /dev/null 2>&1; then
+  echo "fleet image tag $1 does not exist"
+  exit 1
+fi
+
 export FLEET_NETWORKING_AGENT_IMAGE_TAG="$2"
+if [[ $(curl "https://api.github.com/repos/Azure/fleet-networking/tags") != *$2* ]] > /dev/null 2>&1; then
+  echo "fleet networking image tag $2 does not exist"
+  exit 1
+fi
 
 export HUB_CLUSTER="$3"
+if [[ ! $(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$HUB_CLUSTER\")]}") ]] > /dev/null 2>&1; then
+  echo "The cluster named $HUB_CLUSTER does not exist."
+  exit 1
+fi
+
+for MEMBER_CLUSTER in "${@:4}"; do
+if [[ ! $(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$MEMBER_CLUSTER\")]}") ]] > /dev/null 2>&1; then
+  echo "The cluster named $MEMBER_CLUSTER does not exist."
+  exit 1
+fi
+done
+
 export HUB_CLUSTER_CONTEXT=$(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$HUB_CLUSTER\")].name}")
 export HUB_CLUSTER_ADDRESS=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$HUB_CLUSTER\")].cluster.server}")
 
-for MC in "${@:4}"; do
-
-export MEMBER_CLUSTER=$(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$MC\")].name}")
-export MEMBER_CLUSTER_CONTEXT=$(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$MC\")].name}")
+for MEMBER_CLUSTER in "${@:4}"; do
+export MEMBER_CLUSTER_CONTEXT=$(kubectl config view -o jsonpath="{.contexts[?(@.context.cluster==\"$MEMBER_CLUSTER\")].name}")
 
 echo "Switching to member cluster context..."
 kubectl config use-context $MEMBER_CLUSTER_CONTEXT
@@ -36,7 +60,7 @@ echo "Installing mcs-controller-manager..."
 helm install mcs-controller-manager ./charts/mcs-controller-manager/ \
 --set image.repository=$REGISTRY/$MCS_CONTROLLER_MANAGER_IMAGE \
 --set refreshtoken.repository=$REGISTRY/$REFRESH_TOKEN_IMAGE \
---set refreshtoken.tag=$REFRESH_TOKEN_IMAGE_TAG \
+--set refreshtoken.tag=$FLEET_IMAGE_TAG \
 --set image.tag=$FLEET_NETWORKING_AGENT_IMAGE_TAG \
 --set image.pullPolicy=Always \
 --set refreshtoken.pullPolicy=Always \
@@ -53,7 +77,7 @@ echo "Installing member-net-controller-manager..."
 helm install member-net-controller-manager ./charts/member-net-controller-manager/ \
 --set image.repository=$REGISTRY/$MEMBER_NET_CONTROLLER_MANAGER_IMAGE \
 --set refreshtoken.repository=$REGISTRY/$REFRESH_TOKEN_IMAGE \
---set refreshtoken.tag=$REFRESH_TOKEN_IMAGE_TAG \
+--set refreshtoken.tag=$FLEET_IMAGE_TAG \
 --set image.tag=$FLEET_NETWORKING_AGENT_IMAGE_TAG \
 --set timage.pullPolicy=Always \
 --set refreshtoken.pullPolicy=Always \
