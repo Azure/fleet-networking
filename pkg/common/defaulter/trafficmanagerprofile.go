82a2d6468eb7d9c90af10ac877bd3d408d8040ca
Zhiying Lin <zhiyingl456@gmail.com>
2024-09-13 15:07:39 +0800

    address comment

--- pkg/common/defaulter/trafficmanagerprofile.go
+++ pkg/common/defaulter/trafficmanagerprofile.go
@@ -35,10 +35,15 @@ func SetDefaultsTrafficManagerProfile(obj *fleetnetv1alpha1.TrafficManagerProfil
 	}
 
 	// TimeoutInSeconds value depends on the IntervalInSeconds, so that the defaulter MUST handle the IntervalInSeconds first.
+	// * If the Probing Interval is set to 30 seconds, then you can set the Timeout value between 5 and 10 seconds.
+	//   If no value is specified, it uses a default value of 10 seconds.
+	// * If the Probing Interval is set to 10 seconds, then you can set the Timeout value between 5 and 9 seconds.
+	//   If no Timeout value is specified, it uses a default value of 9 seconds.
+	// Reference link: https://learn.microsoft.com/en-us/azure/traffic-manager/traffic-manager-monitoring#configure-endpoint-monitoring
 	if obj.Spec.MonitorConfig.TimeoutInSeconds == nil {
 		if *obj.Spec.MonitorConfig.IntervalInSeconds == 30 {
 			obj.Spec.MonitorConfig.TimeoutInSeconds = ptr.To(int64(10))
-		} else { // assuming validation wehbook should validate the  intervalInSeconds first
+		} else { // assuming validation wehbook should validate the intervalInSeconds first
 			obj.Spec.MonitorConfig.TimeoutInSeconds = ptr.To(int64(9))
 		}
 	}
