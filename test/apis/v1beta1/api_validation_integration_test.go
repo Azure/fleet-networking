877a71a728ff453af97b1fa767f7340b0fa21aca
Britania Rodriguez Reyes <britaniar@microsoft.com>
2025-04-21 08:53:57 -0700

    remove duplicate check

--- test/apis/v1beta1/api_validation_integration_test.go
+++ test/apis/v1beta1/api_validation_integration_test.go
@@ -248,7 +248,6 @@ var _ = Describe("Test networking v1alpha1 API validation", func() {
 			var err = hubClient.Create(ctx, profile)
 			Expect(errors.As(err, &statusErr)).To(BeTrue(), fmt.Sprintf("Create API call produced error %s. Error type wanted is %s.", reflect.TypeOf(err), reflect.TypeOf(&k8serrors.StatusError{})))
 			Expect(statusErr.Status().Message).Should(ContainSubstring("spec.monitorConfig: Invalid value: \"object\": timeoutInSeconds must be between 5 and 9 when intervalInSeconds is 10"))
-			Expect(statusErr.Status().Message).Should(ContainSubstring("timeoutInSeconds must be between 5 and 9 when intervalInSeconds is 10"))
 		})
 
 		It("should deny creating API with timeoutInSeconds < 5 when intervalInSeconds is not defined", func() {
