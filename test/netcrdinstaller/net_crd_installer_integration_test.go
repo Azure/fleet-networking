24de6af0983873786f67cd18f97e4a022fc85557
Arvind Thirumurugan <arvindth@microsoft.com>
2025-07-31 18:05:51 -0700

    make reviewable

--- test/netcrdinstaller/net_crd_installer_integration_test.go
+++ test/netcrdinstaller/net_crd_installer_integration_test.go
@@ -8,8 +8,8 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/apimachinery/pkg/types"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 
 	cmdNetCRDInstaller "go.goms.io/fleet-networking/cmd/net-crd-installer/utils"
 )
